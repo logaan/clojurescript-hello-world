7dd5eb8a2ba2ad7274cb07e5a8709bf05a84ba39
Logan Campbell-McPherson <colin@logaan.net>
2012-05-12 00:16:30 +1000

    fixed project

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject engine-scheduler-interface "0.0.1"
-  :description "A simple interface for the scheduler engine"
+(defproject hello-world "0.0.1"
+  :description "Hello world will never go out of style. It will never leave you and take your pets."
   :dependencies [[org.clojure/clojure "1.4.0"]]
   :plugins [[lein-cljsbuild "0.1.9"]]
   :cljsbuild {
